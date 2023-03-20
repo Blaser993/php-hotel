ae21afd1d8e9085aa5714904bc816f3580fe3ce7
Blaser993 <corradodepinto.design@gmail.com>
2023-03-20 15:35:18 +0100

    PUSH PRE TICKET

--- index.php
+++ index.php
@@ -41,3 +41,21 @@
     ];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP hotels</title>
+</head>
+<body>
+
+    <?php
+    foreach ($hotels as $key => $hotel):
+        echo $hotel;
+    endforeach;    
+    ?>
+</body>
+</html>
